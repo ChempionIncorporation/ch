78155afb6de065826baa475d3495176f84e56fa4
= <tilianlol@gmail.com>
2016-06-29 14:12:38 +0300

    Artem: connect Version: 1.5.0.5

--- pages/services/header.php
+++ pages/services/header.php
@@ -2,7 +2,7 @@
 <?
 function connect()
 {
-    $conn = mysql_connect("localhost", "root", "") or die(mysql_error());
+    $conn = mysql_connect("champ00.mysql.ukraine.com.ua", "champ00_db", "2UWXvGb2") or die(mysql_error());
     $tbl = mysql_select_db("champ00_db", $conn) or die(mysql_error());
     mysql_query("SET NAMES utf8");
     return $tbl;
