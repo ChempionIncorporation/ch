e8ade7dcb4f3394dcb31f0be0c395cccdb070344
Gleb <web_champ@ukr.net>
2016-06-29 14:37:34 +0300

    Artem: connect Verstion: 1.5.0.8

--- modules/footer.php
+++ modules/footer.php
@@ -1,57 +1,59 @@
-<footer class="container-fluid text-center"
-        style="bottom:0%;position:absolute; bottom: 0px; width:100%;background: #88212a;">
-    <div class="row" style="background: #88212a;color: white; height:auto;">
-        <div class="col-xs-3">
-            <div class="box" style="padding:5px">
-                &copy;&nbspРекламное агенство "Чемпион"<br/>г. Одесса, ул. Княжеская, 3/5
+<div class="container">
+    <footer class="footer navbar-bottom text-center"
+            style="background: #88212a;">
+        <div class="row" style="background: #88212a;color: white; height:auto;">
+            <div class="col-xs-3">
+                <div class="box" style="padding:5px">
+                    &copy;&nbspРекламное агенство "Чемпион"<br/>г. Одесса, ул. Княжеская, 3/5
+                </div>
+            </div>
+            <div class="col-xs-3 start-xs">
+                <div class="box" style="font-size:10pt;">
+                    <span class="glyphicon glyphicon-earphone"></span>
+                    Позвонить нам<br/>
+                    (048) 795-68-03 / (048) 736-26-49<br/>
+                    </span>
+                    (093) 946-14-95 / (095) 862-61-64 <br/>
+                    (096) 321-58-11<br/>
+                    <span class="glyphicon glyphicon-envelope"></span>
+                    7362649@ukr.net
+                </div>
+            </div>
+            <div class="col-xs start-xs">
+                <div class="box">
+                    <img src="/assets/img/footer-logo.png" style="padding:2px;">
+                    <img src="/assets/img/social/1/vk.png"
+                         id="123" style="padding-bottom:20px;"
+                         onMouseUp="document.getElementById(123).src = '/assets/img/social/2/vk.png'"
+                         onMouseOver="document.getElementById(123).src = '/assets/img/social/2/vk.png'"
+                         onMouseOut="document.getElementById(123).src = '/assets/img/social/1/vk.png'"
+                    >
+                    <img src="/assets/img/social/1/tw.png"
+                         id="124" style="padding-bottom:20px;"
+                         onMouseUp="document.getElementById(124).src = '/assets/img/social/2/tw.png'"
+                         onMouseOver="document.getElementById(124).src = '/assets/img/social/2/tw.png'"
+                         onMouseOut="document.getElementById(124).src = '/assets/img/social/1/tw.png'"
+                    >
+                    <img src="/assets/img/social/1/fb.png"
+                         id="125" style="padding-bottom:20px;"
+                         onMouseUp="document.getElementById(125).src = '/assets/img/social/2/fb.png'"
+                         onMouseOver="document.getElementById(125).src = '/assets/img/social/2/fb.png'"
+                         onMouseOut="document.getElementById(125).src = '/assets/img/social/1/fb.png'"
+                    >
+                    <img src="/assets/img/social/1/gp.png"
+                         id="126" style="padding-bottom:20px;"
+                         onMouseUp="document.getElementById(126).src = '/assets/img/social/2/gp.png'"
+                         onMouseOver="document.getElementById(126).src = '/assets/img/social/2/gp.png'"
+                         onMouseOut="document.getElementById(126).src = '/assets/img/social/1/gp.png'"
+                    >
+                </div>
+            </div>
+            <div class="col-xs-3 end-xs">
+                <div class="box">
+                    v1.1005
+                </div>
             </div>
         </div>
-        <div class="col-xs-3 start-xs">
-            <div class="box" style="font-size:10pt;">
-                <span class="glyphicon glyphicon-earphone"></span>
-                Позвонить нам<br/>
-                (048) 795-68-03 / (048) 736-26-49<br/>
-                </span>
-                (093) 946-14-95 / (095) 862-61-64 <br/>
-                (096) 321-58-11<br/>
-                <span class="glyphicon glyphicon-envelope"></span>
-                7362649@ukr.net
-            </div>
-        </div>
-        <div class="col-xs start-xs">
-            <div class="box">
-                <img src="/assets/img/footer-logo.png" style="padding:2px;">
-                <img src="/assets/img/social/1/vk.png"
-                     id="123" style="padding-bottom:20px;"
-                     onMouseUp="document.getElementById(123).src = '/assets/img/social/2/vk.png'"
-                     onMouseOver="document.getElementById(123).src = '/assets/img/social/2/vk.png'"
-                     onMouseOut="document.getElementById(123).src = '/assets/img/social/1/vk.png'"
-                >
-                <img src="/assets/img/social/1/tw.png"
-                     id="124" style="padding-bottom:20px;"
-                     onMouseUp="document.getElementById(124).src = '/assets/img/social/2/tw.png'"
-                     onMouseOver="document.getElementById(124).src = '/assets/img/social/2/tw.png'"
-                     onMouseOut="document.getElementById(124).src = '/assets/img/social/1/tw.png'"
-                >
-                <img src="/assets/img/social/1/fb.png"
-                     id="125" style="padding-bottom:20px;"
-                     onMouseUp="document.getElementById(125).src = '/assets/img/social/2/fb.png'"
-                     onMouseOver="document.getElementById(125).src = '/assets/img/social/2/fb.png'"
-                     onMouseOut="document.getElementById(125).src = '/assets/img/social/1/fb.png'"
-                >
-                <img src="/assets/img/social/1/gp.png"
-                     id="126" style="padding-bottom:20px;"
-                     onMouseUp="document.getElementById(126).src = '/assets/img/social/2/gp.png'"
-                     onMouseOver="document.getElementById(126).src = '/assets/img/social/2/gp.png'"
-                     onMouseOut="document.getElementById(126).src = '/assets/img/social/1/gp.png'"
-                >
-            </div>
-        </div>
-        <div class="col-xs-3 end-xs">
-            <div class="box">
-                v1.1005
-            </div>
-        </div>
-    </div>
-</footer>
+    </footer>
+</div>
 
