0d433fd459b8a374953463e5d7144bc73d212372
Gleb <web_champ@ukr.net>
2016-06-29 15:30:21 +0300

    Gleb: connect Verstion: 1.5.0.А

--- pages/services/shop/modules/functions.php
+++ pages/services/shop/modules/functions.php
@@ -9,13 +9,6 @@ if($_SESSION['gleb'] == null || $_SESSION['login'] == "" || $_SESSION['login'] =
         $_SESSION['cart'][255]  = "";
 }
 //
-function connect()
-{
-    $conn = mysql_connect("champ00.mysql.ukraine.com.ua", "champ00_db", "2UWXvGb2") or die(mysql_error());
-    $tbl = mysql_select_db("champ00_db", $conn) or die(mysql_error());
-    mysql_query("SET NAMES utf8");
-    return $tbl;
-}
 
 //
 function lat($st)
